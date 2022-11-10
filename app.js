91bfde7870ae929d9c5d5542f5cd50131a7335e1
Mstyslav Dmytryk <mstyslavdmytryk@gmail.com>
2022-11-10 15:44:36 +0200

    final version

--- app.js
+++ app.js
@@ -1,64 +1,11 @@
-const prev = document.querySelector("#prev");
-const next = document.querySelector("#next");
-const progress = document.querySelector("#progress");
-const circles = document.querySelectorAll(".circle");
+const openButton = document.querySelector('#open')
+const closeButton = document.querySelector('#close')
+const container = document.querySelector('.container')
 
-let currentActive = 1;
+openButton.addEventListener('click', ()=> {
+  container.classList.add('show-nav')
+})
 
-prev.addEventListener("click", () => {
-  currentActive--;
-  console.log(currentActive);
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  update();
-});
-
-next.addEventListener("click", () => {
-  if (currentActive >= circles.length) {
-    return
-  }
-
-  currentActive++;
-  console.log(currentActive);
-
-  update();
-});
-
-function update() {
-  circles.forEach((circle, index) => {
-    if (index < currentActive) {
-      circle.classList.add("active");
-      console.log("circle index:" + index)
-    } else {
-      circle.classList.remove("active");
-    }
-  });
-
-  const actives = document.querySelectorAll(".active");
-
-  switch (actives.length) {
-    case 1:
-      progress.style.width = "0%";
-      break;
-    case 2:
-      progress.style.width = "33%";
-      break;
-    case 3:
-      progress.style.width = "66%";
-      break;
-    case 4:
-      progress.style.width = "100%";
-      break;
-  }
-
-  if (currentActive === 1) {
-    prev.disabled = true;
-  } else if (currentActive === 4) {
-    next.disabled = true;
-  } else {
-    next.disabled = false;
-    prev.disabled = false;
-  }
-}
+closeButton.addEventListener('click', ()=> {
+  container.classList.remove('show-nav')
+})
