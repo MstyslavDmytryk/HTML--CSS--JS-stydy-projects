22fbdccef990cb5405848c69e8352485d35b37eb
Mstyslav Dmytryk <mstyslavdmytryk@gmail.com>
2022-11-14 11:54:02 +0200

    html,css update

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const openButton = document.querySelector('#open')
-const closeButton = document.querySelector('#close')
-const container = document.querySelector('.container')
-
-openButton.addEventListener('click', ()=> {
-  container.classList.add('show-nav')
-})
-
-closeButton.addEventListener('click', ()=> {
-  container.classList.remove('show-nav')
-})
+// todo
